678ddfe518870d949df2436e639994ba1de6005a
Mara-185 <marastefania.calo@gmail.com>
2023-08-11 11:14:18 +0200

    ToT linearity all FE

--- Analysis/W14R12/C_inj/c_inj_all.py
+++ Analysis/W14R12/C_inj/c_inj_all.py
@@ -39,20 +39,83 @@ FRONTENDS_PEAK = [
     ([22.28, 53.21, 66.13, 0, 79.45, 99.3]),
     ([24.35, 59.06, 73.3,0, 86.4, 0])]
 
+ELECTRONS = [
+    1616,
+    3808,
+    4849,
+    5671,
+    6027,
+    7233,
+]
+
 def func_norm(x,a,b,c,t, th):
     return np.where(x<th, 0, np.maximum(0, a*x+b-(c/(x-t))))
 
 if __name__ == "__main__":
-    i=1
-    for (a,b,c,t,name), thr, peak in zip(FRONTENDS_PARAM_NO, FRONTENDS_PARAM_TH, FRONTENDS_PEAK):
-        print(f"{name} FE:\n")
-        print(peak)
 
-        for p in peak:
-            print(f"peak_tot = {p}")
-            tot_dac1 = (t/2)-(b/(2*a))+(p/(2*a)) + np.sqrt(((t/2)+(b/(2*a))-(p/(2*a)))**2 + (c/a))
-            tot_dac2 = (t/2)-(b/(2*a))+(p/(2*a)) - np.sqrt(((t/2)+(b/(2*a))-(p/(2*a)))**2 + (c/a))
+    peaks = np.zeros((4, 6),dtype=float)
+    tot_dac = np.zeros((4, 6),dtype=float)
+    c_from_inj = np.zeros((4, 6),dtype=float)
+    c_from_inj_mean = np.zeros((4, 1),dtype=float)
+    new_dac = np.zeros((4, 6),dtype=float)
 
-            print(f"{i}_+ = {tot_dac1}\n")
-            print(f"{i}_- = {tot_dac2}\n")
-            i+=1
+    with open("cap_from_injection.txt", "w+") as outf:
+        for j,((a,b,c,t,name), thr, peak) in enumerate(zip(FRONTENDS_PARAM_NO, FRONTENDS_PARAM_TH, FRONTENDS_PEAK)):
+            print(f"{name} FE:\n")
+            print(f"{name} FE:\n{peak}", file=outf)
+            print(peak)
+            c_sum=0
+
+            for i,(p,el) in enumerate(zip(peak, ELECTRONS)):
+                peaks[j,i] = p
+                print(f"peak_tot = {p}", file=outf)
+                print(f"peak_tot = {p}")
+                tot_dac1 = (t/2)-(b/(2*a))+(p/(2*a)) + np.sqrt(((t/2)+(b/(2*a))-(p/(2*a)))**2 + (c/a))
+                #tot_dac2 = (t/2)-(b/(2*a))+(p/(2*a)) - np.sqrt(((t/2)+(b/(2*a))-(p/(2*a)))**2 + (c/a))
+
+                print(f"{i+1}_+ = {tot_dac1}\n")
+                print(f"{i+1}_+ = {tot_dac1}", file=outf)
+                #print(f"{i}_- = {tot_dac2}\n")
+                c_el = el/tot_dac1
+                # c_sum+=c_el
+
+                print(f"    C = {c_el}")
+                print(f"    C = {c_el}", file=outf)
+                if p!=0:
+                    tot_dac[j, i] = tot_dac1
+                    c_from_inj[j,i] = c_el
+                    c_sum+=c_el
+
+                plt.hist(c_from_inj[j])
+                plt.savefig(f"c_from_inj_ditribution_{name}.png")
+                plt.clf()
+
+
+            n = np.count_nonzero(peak)
+            c_mean=c_sum/n
+            c_from_inj_mean[j] = c_mean
+            print(n, c_mean, c_from_inj_mean)
+
+
+
+    print(peaks)
+    print(tot_dac)
+    print(c_from_inj)
+
+    for k, z in enumerate(c_from_inj_mean):
+        for l,e in enumerate(ELECTRONS):
+            new_dac[k,l] = e/z
+
+    print(new_dac)
+
+
+
+    # Creating *.npz file with three field, each one of them is a matrix of four
+    # rows corresponding to the FE and 6 columns (sources peaks):
+    np.savez_compressed(
+        "cap_from_injection.npz",
+        peaks_inj = peaks,
+        tot_dac_inj = tot_dac,
+        c_from_inj = c_from_inj,
+        c_from_inj_means = c_from_inj_mean)
+    print("\"*.npz\" file is created.")
--- Analysis/W14R12/linearity_tot/linearity.py
+++ Analysis/W14R12/linearity_tot/linearity.py
@@ -26,15 +26,26 @@ FRONTENDS_PEAK = [
     ([24.35, 59.06, 73.3,0, 86.4, 0])]
 
 FRONTENDS_DAC1 = [
+    # OBTAINED CONSIDERING 10.1 e/DAC
     # Fe , Am1, Am2, Am3 ,Cd, Am4, name
     np.array([160, 377, 480, 561, 597, 716])]
 
 FRONTENDS_DAC2 = [
+    # CONSIDERING CALIBRATION FROM IRON55
     # Fe , Am1, Am2, Am3 ,Cd, Am4, name
     ([176, 414, 527, 616, 655, 786]),
     ([190, 448, 571, 667, 709, 850]),
     ([87, 206, 262, 307, 326, 391]),
     ([109, 256, 325, 381, 405, 485])]
+
+FRONTENDS_DAC3 = [
+    # CONSIDERING CALIBRATION FROM TOT-Q RELATIONSHIP
+    # AND PEAKS SOURCES (MEANS OF ALL RESULTS)
+    # Fe , Am1, Am2, Am3 ,Cd, Am4, name
+    ([175, 411, 524, 613, 651, 781]),
+    ([174, 409, 521, 609, 647, 777]),
+    ([83, 195, 248, 291, 309, 371]),
+    ([86, 203, 259, 302, 321, 386])]
 
 
 FRONTENDS_PARAM_NO = [
@@ -58,6 +69,21 @@ FRONTENDS_PARAM_TH= [
     (35.34),
     (31.7)]
 
+FRONT_CAL_IRON =[
+    (9.2),
+    (8.5),
+    (18.5),
+    (14.9)
+]
+
+FRONT_CAL_SOURCES =[
+    (9.26),
+    (9.31),
+    (19.51),
+    (18.75)
+]
+
+
 
 
 
@@ -71,7 +97,7 @@ if __name__ == "__main__":
 
     dac = FRONTENDS_DAC1[0]
 
-
+    # CALIBRATION 10-1 e-/DAC (EACH FE & ALL)
     #for name in ["Normal", "Cascode", "HV Casc.", "HV"]:
     #for (a,b,c,t,name), p,thr, dac, poi, color in zip(FRONTENDS_PARAM_NO, FRONTENDS_PEAK, FRONTENDS_PARAM_TH, POINT, COL):
     for (a,b,c,t,name), p,thr, point in zip(FRONTENDS_PARAM_NO, FRONTENDS_PEAK, FRONTENDS_PARAM_TH, MARKER):
@@ -88,15 +114,17 @@ if __name__ == "__main__":
         plt.xlim([0, 1200])
         plt.ylim([0, 128])
         plt.suptitle(f"ToT curve linearity")
+        plt.title(f"{name} - C = 10.1 e-/DAC")
         plt.xlabel("True injected charge [DAC]")
         plt.ylabel("ToT [25 ns]")
-        plt.savefig(f"ToT_{name}.png")
+        plt.savefig(f"ToT_{name}_thesis.png")
         plt.clf()
         # set_integer_ticks(plt.gca().xaxis, plt.gca().yaxis)
         # cb = integer_ticks_colorbar()
         # cb.set_label("Hits / bin")
         #plt.legend(loc="upper left")
 
+    # ALL
     for (a,b,c,t,name), p,thr, point in zip(FRONTENDS_PARAM_NO, FRONTENDS_PEAK, FRONTENDS_PARAM_TH, MARKER):
         print(name)
         print(dac)
@@ -112,15 +140,17 @@ if __name__ == "__main__":
     plt.xlim([0, 1200])
     plt.ylim([0, 128])
     plt.suptitle(f"ToT curve linearity")
+    plt.title(f"All FE - C = 10.1 e-/DAC")
     plt.xlabel("True injected charge [DAC]")
     plt.ylabel("ToT [25 ns]")
-    plt.savefig(f"Tot_linearity_1.png")
+    plt.savefig(f"Tot_linearity_thesis.png")
     plt.clf()
 
     del a, b, c, t, name, p, thr, dac, point
 
 
-    for (a,b,c,t,name), p,thr, dac, point in zip(FRONTENDS_PARAM_NO, FRONTENDS_PEAK, FRONTENDS_PARAM_TH, FRONTENDS_DAC2, MARKER):
+    # CALIBRATION FROM IRON PEAKS (EACH FE &ALL)
+    for (a,b,c,t,name), p,thr, dac, point, calcap in zip(FRONTENDS_PARAM_NO, FRONTENDS_PEAK, FRONTENDS_PARAM_TH, FRONTENDS_DAC2, MARKER,FRONT_CAL_IRON):
         print(dac)
         print(p)
         print(thr)
@@ -135,17 +165,40 @@ if __name__ == "__main__":
         plt.xlim([0, 1200])
         plt.ylim([0, 128])
         plt.suptitle(f"ToT curve linearity")
+        plt.title(f"{name} - C = {calcap} e-/DAC (from Fe55)")
         plt.xlabel("True injected charge [DAC]")
         plt.ylabel("ToT [25 ns]")
-        plt.savefig(f"ToT_{name}2.png")
+        plt.savefig(f"ToT_{name}_iron.png")
         plt.clf()
         # set_integer_ticks(plt.gca().xaxis, plt.gca().yaxis)
         # cb = integer_ticks_colorbar()
         # cb.set_label("Hits / bin")
         #plt.legend(loc="upper left")
 
+        # ALL
+    for (a,b,c,t,name), p,thr, dac, point, calcap in zip(FRONTENDS_PARAM_NO, FRONTENDS_PEAK, FRONTENDS_PARAM_TH, FRONTENDS_DAC2, MARKER, FRONT_CAL_IRON):
+        print(dac)
+        print(p)
+        print(thr)
+        print(a, b, c, t)
 
-    for (a,b,c,t,name), p,thr, dac, point in zip(FRONTENDS_PARAM_NO, FRONTENDS_PEAK, FRONTENDS_PARAM_TH, FRONTENDS_DAC2, MARKER):
+        plt.plot(np.asarray(dac), np.asarray(p), f"{point}")
+        y =  np.arange(thr, 1000, 1)
+        plt.plot(y, func_norm(y, a, b, c, t, thr), f"--")
+
+    plt.xlim([0, 1200])
+    plt.ylim([0, 128])
+    plt.suptitle(f"ToT curve linearity")
+    plt.title(f"All FE - C = {calcap} e-/DAC (from Fe55)")
+    plt.xlabel("True injected charge [DAC]")
+    plt.ylabel("ToT [25 ns]")
+    plt.savefig(f"Tot_linearity_iron.png")
+    plt.clf()
+
+    del a, b, c, t, name, p, thr, dac, point
+
+    # CALIBRATION FROM SOURCES PEAKS MEANS (EACH FE &ALL)
+    for (a,b,c,t,name), p,thr, dac, point, calcap2 in zip(FRONTENDS_PARAM_NO, FRONTENDS_PEAK, FRONTENDS_PARAM_TH, FRONTENDS_DAC3, MARKER, FRONT_CAL_SOURCES):
         print(dac)
         print(p)
         print(thr)
@@ -156,9 +209,36 @@ if __name__ == "__main__":
         plt.plot(y, func_norm(y, a, b, c, t, thr), f"--")
 
 
+
+        plt.xlim([0, 1200])
+        plt.ylim([0, 128])
+        plt.suptitle(f"ToT curve linearity")
+        plt.title(f"{name} - C = {calcap2} e-/DAC (from all sources)")
+        plt.xlabel("True injected charge [DAC]")
+        plt.ylabel("ToT [25 ns]")
+        plt.savefig(f"ToT_{name}_sources.png")
+        plt.clf()
+        # set_integer_ticks(plt.gca().xaxis, plt.gca().yaxis)
+        # cb = integer_ticks_colorbar()
+        # cb.set_label("Hits / bin")
+        #plt.legend(loc="upper left")
+
+        # ALL
+    for (a,b,c,t,name), p,thr, dac, point, calcap2 in zip(FRONTENDS_PARAM_NO, FRONTENDS_PEAK, FRONTENDS_PARAM_TH, FRONTENDS_DAC3, MARKER, FRONT_CAL_SOURCES):
+        print(dac)
+        print(p)
+        print(thr)
+        print(a, b, c, t)
+
+        plt.plot(np.asarray(dac), np.asarray(p), f"{point}")
+        y =  np.arange(thr, 1000, 1)
+        plt.plot(y, func_norm(y, a, b, c, t, thr), f"--")
+
     plt.xlim([0, 1200])
     plt.ylim([0, 128])
     plt.suptitle(f"ToT curve linearity")
+    plt.title(f"All FE - C = {calcap2} e-/DAC (from all sources)")
     plt.xlabel("True injected charge [DAC]")
     plt.ylabel("ToT [25 ns]")
-    plt.savefig(f"Tot_linearity_2.png")
+    plt.savefig(f"Tot_linearity__sources.png")
+    plt.clf()
